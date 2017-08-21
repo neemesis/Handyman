f3a3a89cdfcfb582917c0f5772c5826d0d17dfa8
Mirche Toshevski <mirche.toshevski@gmail.com>
2017-08-21 16:00:52 +0200

    SysPlugin basic commands

--- Slave.SysPugin/SysPlugin.cs
+++ Slave.SysPugin/SysPlugin.cs
@@ -1,9 +1,15 @@
 ﻿using Slave.Framework.Interfaces;
 using System;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace Slave.SysPugin {
     public class SysPlugin : IMaster {
+        public SysPlugin() {
+            _mAlias = "sys";
+            _mHotKey = Shortcut.None;
+        }
+
         public string Name => "System Plugin";
         public string Description => "Execute system commands.";
         public string Author => "Mirche Toshevski";
@@ -27,15 +33,34 @@ namespace Slave.SysPugin {
         public IParse Parser { get; set; }
 
         public void Execute(string[] args, Action<string> display = null) {
+            var cmdArgs = "shutdown ";
             if (args[0] == "shutdown") {
-
+                if (args.Length == 1) {
+                    cmdArgs += "/s";
+                } else {
+                    var mins = int.Parse(args[1]);
+                    cmdArgs += "/s /t " + (mins * 60).ToString();
+                } 
             } else if (args[0] == "restart") {
-
-            } else if (args[0] == "sleep") {
-
+                cmdArgs += "/r";
             } else if (args[0] == "hibernate") {
-
+                cmdArgs += "/h";
+            } else if (args[0] == "abort") {
+                cmdArgs += "/a";
+            } else if (args[0] == "logoff") {
+                cmdArgs += "/l";
             }
+
+            var startInfo = new ProcessStartInfo {
+                FileName = @"cmd.exe",
+                Arguments = cmdArgs,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            var process = new Process { StartInfo = startInfo };
+            process.Start();
         }
 
         public void Initialize() {
