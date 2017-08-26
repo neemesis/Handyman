5055f955cbbe712d440ec39eb83a2aada6c8d7a6
Mirche Toshevski <mirche.toshevski@gmail.com>
2017-08-26 23:55:23 +0200

    TranslatorPlugin

---
+++ Slave.TranslatorPlugin/TranslatorParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Slave.Framework.Interfaces;
+
+namespace Slave.TranslatorPlugin {
+    public class TranslatorParser : IParse {
+        public string[] Parse(string str) {
+            return str.Split(' ');
+        }
+    }
+}
